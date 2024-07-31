c5632651539167f62da0c59e1bcc7533d7aca96d
dawidtanski <33099962+dawidtanski@users.noreply.github.com>
2024-07-31 19:25:13 +0200

    Add files via upload

---
+++ functions/print_capital_hexa.c
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   print_capital_hexa.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dtanski <dtanski@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/05 13:35:31 by dtanski           #+#    #+#             */
+/*   Updated: 2024/05/06 09:38:02 by dtanski          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../ft_printf.h"
+
+static int	ft_get_size(unsigned int n)
+{
+	int	size;
+
+	size = 0;
+	if (n <= 0)
+		size++;
+	while (n != 0)
+	{
+		n = n / 16;
+		size++;
+	}
+	return (size);
+}
+
+static void	ft_fill_res(int size, int offset, unsigned int n, char *res)
+{
+	int	digit;
+
+    while (size > offset)
+    {
+        digit = n % 16;
+        if (digit < 10)
+            res[size - 1] = digit + '0';
+        else
+            res[size - 1] = (digit - 10) + 'A';
+        n /= 16;
+        size--;
+    }
+}
+
+int	print_capital_hexa(unsigned int n)
+{
+	int		offset;
+	int		size;
+	char	*res;
+
+	offset = 0;
+	size = ft_get_size(n);
+	res = (char *)malloc(sizeof(char) * size + 1);
+	if (!res)
+		return (0);
+	if (n < 0)
+	{
+		res[0] = '-';
+		offset = 1;
+		n = -n;
+	}
+	ft_fill_res(size, offset, n, res);
+	res[size] = '\0';
+	print_str(res);
+	free(res);
+	return (size);
+}
